1dc72b559194c2895a7fc2f5dbb7e5fdaca83346
Andy Eccleston <eandy5000@yahoo.com>
2017-10-01 20:20:39 -0500

    created several function varitions to create a list of strings to the dom

--- another.elm
+++ another.elm
@@ -18,15 +18,40 @@ stati =
     , checkStatus 418
     ]
 
+--one way to render a list
 renderList: List String -> Html msg
 renderList list =
-    ul [] 
-    (List.map(\ i -> li [] [text i]) list)
+    list
+    |> (List.map(\i -> li [] []))
+    |> ul []
+
+--second way to render a list
+renderList2: List String -> Html msg
+renderList2 list =
+    ul []
+    (List.map(\i -> li [] [text i]) list)
+
+--third way using helper function
+renderList3: List String -> Html msg  
+renderList3 list =
+    ul []
+    (List.map (liMaker) list)
+
+--third way with helper variation
+renderList4: List String -> Html msg
+renderList4 list =
+    list
+    |> List.map liMaker
+    |> ul []
     
+--helper function takes String -> li
+liMaker: String -> Html msg  
+liMaker str =
+    li [] [text str]
 
 main: Html msg
 main = 
     div [] [
         h1 [] [text "Maps and Lists"]
-        , renderList stati
+        , renderList4 stati
     ]
